1f18aa36e4fa92ccb74471a77e60dbfb04502a73
aagr8868 <aagr8868@colorado.edu>
2023-02-25 19:12:35 -0700

    created helper class with simple helper tool functions

---
+++ Project3/src/Helper.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+
+public class Helper {
+    static int uniqueID = 0;
+    static int RandInt(int min, int max){
+        Random rn = new Random();
+        return rn. nextInt(max - min + 1) + min;
+    }
+    static boolean PercentChance(int chance_to_pass){
+        return RandInt(1, 100) <= chance_to_pass;
+    }
+    static int GenerateUniqueID () {
+        uniqueID += RandInt(0, 9);
+        return uniqueID;
+    }
+}
