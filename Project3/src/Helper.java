d470b138b147ec8ba82ead5a22c7f55c29341040
aagr8868 <aagr8868@colorado.edu>
2023-02-28 01:00:52 -0700

    added in descriptions of functions

--- Project3/src/Helper.java
+++ Project3/src/Helper.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:195ad2600865394bb3f6dc45f8770ecdca3c25339759684f94bcb2e8e0661db5
-size 422
+oid sha256:6940fcb3d454dd1c297a8bf702fd51c408642d54c1aab7f55863e1c50a739ec7
+size 895
