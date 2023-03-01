adc2c5dc88d6dca00840b6eeadc2fe220314f016
aagr8868 <aagr8868@colorado.edu>
2023-02-28 20:17:39 -0700

    bug: Mechanics sometimes dont repair a car wven when they should

--- Project3/src/FNCD.java
+++ Project3/src/FNCD.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b99a6c587df16ec1315341b1ef0999dad6823418fca80ba4a88e748b6b1b1278
-size 18918
+oid sha256:19a96ad2afbc381664947dd9befc75ceb557ea0adeebc218ac5c8c47e54fd6a4
+size 19268
--- Project3/src/Staff.java
+++ Project3/src/Staff.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4c0d1465507074022c57d69fc235972bb06161f43130cc6633d554d13f956ba0
-size 3455
+oid sha256:ffdf61c6281424b53e6111f232f9199223ffbdcb061a1fdebd4b2adda8c095a2
+size 3379
