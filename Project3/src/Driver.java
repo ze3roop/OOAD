9877ff8cb0b9d9cd26e57f093aaa75675b745f92
aagr8868 <aagr8868@colorado.edu>
2023-03-02 13:03:24 -0700

    added basic driver class

---
+++ Project3/src/Driver.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c75c0f87d99f297468c2adeb25d8184977192d1af64bc0cd7e66452518daf6d8
+size 325
--- Project3/src/FNCD.java
+++ Project3/src/FNCD.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0b97a94e5a9b4d15d2d4de6f426cc66e517ac7953065eefb9bb098caac1937cd
-size 19598
+oid sha256:25e2c767764f8cc993dbaf5b9b5064a5e87309cdbf29949b6ec1b43ab29c03d9
+size 19891
