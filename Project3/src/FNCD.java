a05d412ccd2a38fb350d9926dc004798392dc0a7
ze3roop <evanthompson1458@gmail.com>
2023-03-04 00:02:42 -0700

    whoops wrong submit

--- Project3/src/FNCD.java
+++ Project3/src/FNCD.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b95a0b0409c1748e8b84aba2b1760358799f4ce17b4a6ccbaa8f87a6a35c28a7
-size 35427
+oid sha256:efd08730d814229c312784cadca0ce786322b9b6a21ee0396da6016da8a0f1f3
+size 35433
