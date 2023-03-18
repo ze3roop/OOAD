51081882e88c13a3b44ff83bce2663bf42098bee
ze3roop <evanthompson1458@gmail.com>
2023-03-18 14:28:32 -0600

    Singleton finished including lazy and eager

--- Project3/src/FNCD.java
+++ Project3/src/FNCD.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:efd08730d814229c312784cadca0ce786322b9b6a21ee0396da6016da8a0f1f3
-size 35433
+oid sha256:c9da57198562b43762c7e3fab95af3bdea38bcf47cb3c9258fcc98f78aabee18
+size 35902
